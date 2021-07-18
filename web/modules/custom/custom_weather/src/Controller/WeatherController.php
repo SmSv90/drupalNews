c696fe63ac222500e2dd7de9acfa2bd38e2fc210
Sviatoslav Smovdyr <smr.stv@gmail.com>
2021-07-18 14:53:45 +0300

    feat: Add custom weather block

--- web/modules/custom/custom_weather/src/Controller/WeatherController.php
+++ web/modules/custom/custom_weather/src/Controller/WeatherController.php
@@ -7,8 +7,6 @@ namespace Drupal\custom_weather\Controller;
 class WeatherController {
   public function content() {
     return array(
-      '#type' => 'markup',
-      '#markup' => t('Hello, World! It is custom weather!'),
     );
   }
 }
--- web/modules/custom/custom_weather/src/Plugin/Block/WeatherBlock.php
+++ web/modules/custom/custom_weather/src/Plugin/Block/WeatherBlock.php
@@ -19,9 +19,50 @@ class WeatherBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function build() {
+
+    $client = \Drupal::httpClient();
+    $request = $client->get('api.openweathermap.org/data/2.5/weather?q=Lutsk&units=metric&appid=d0c33cd5a7fb741a9d1f0f6db947d48e');
+    $response = $request->getBody()->getContents();
+    $responseParsed = json_decode($response, true);
+
+    $cityName = [
+      '#type' => 'html_tag',
+      '#tag' => 'p',
+      '#value' => 'Location: ' . $responseParsed['name']
+    ];
+
+    $temperatureValue = [
+      '#type' => 'html_tag',
+      '#tag' => 'p',
+      '#value' => 'Temperature: ' . $responseParsed['main']['temp'] . ' Celsius'
+    ];
+
+    $humidityValue= [
+      '#type' => 'html_tag',
+      '#tag' => 'p',
+      '#value' => 'Humidity: ' . $responseParsed['main']['humidity'] . ' %'
+    ];
+
+    $weatherValue= [
+      '#type' => 'html_tag',
+      '#tag' => 'p',
+      '#value' => 'Weather: ' . $responseParsed['weather'][0]['description']
+    ];
+
+    $windValue= [
+      '#type' => 'html_tag',
+      '#tag' => 'p',
+      '#value' => 'Wind: ' . $responseParsed['wind']['speed'] . ' m/s'
+    ];
+
     return [
-      '#markup' => $this->t('Hello, World! It is custom weather!'),
+      $cityName,
+      $weatherValue,
+      $temperatureValue,
+      $humidityValue,
+      $windValue,
     ];
   }
 
 }
+
