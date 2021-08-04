45ab218721fc4c3fb4da8d63e4b93aba3bb3801b
Sviatoslav Smovdyr <smr.stv@gmail.com>
2021-08-04 15:10:37 +0300

    feat: Add CSS

--- web/modules/custom/custom_weather/src/Plugin/Block/WeatherBlockUA.php
+++ web/modules/custom/custom_weather/src/Plugin/Block/WeatherBlockUA.php
@@ -13,56 +13,56 @@ use Drupal\Core\Block\BlockBase;
  *   category = @Translation("Custom Weather"),
  * )
  */
-class WeatherBlockUA extends BlockBase {
+class WeatherBlockUA extends BlockBase
+{
 
   /**
    * {@inheritdoc}
    */
-  public function build() {
+    public function build()
+    {
+        $url ='api.openweathermap.org/data/2.5/weather?q=Lutsk&units=metric&appid=d0c33cd5a7fb741a9d1f0f6db947d48e&lang=ua';
+        $client = \Drupal::httpClient();
+        $request = $client->get($url);
+        $response = $request->getBody()->getContents();
+        $responseParsed = json_decode($response, true);
 
-    $client = \Drupal::httpClient();
-    $request = $client->get('api.openweathermap.org/data/2.5/weather?q=Lutsk&units=metric&appid=d0c33cd5a7fb741a9d1f0f6db947d48e&lang=ua');
-    $response = $request->getBody()->getContents();
-    $responseParsed = json_decode($response, true);
+        $cityName = [
+        '#type' => 'html_tag',
+        '#tag' => 'p',
+        '#value' => 'Локація: ' . $responseParsed['name']
+        ];
 
-    $cityName = [
-      '#type' => 'html_tag',
-      '#tag' => 'p',
-      '#value' => 'Локація: ' . $responseParsed['name']
-    ];
+        $temperatureValue = [
+        '#type' => 'html_tag',
+        '#tag' => 'p',
+        '#value' => 'Температура: ' . round($responseParsed['main']['temp']) . '&#x2103;'
+        ];
 
-    $temperatureValue = [
-      '#type' => 'html_tag',
-      '#tag' => 'p',
-      '#value' => 'Температура: ' . $responseParsed['main']['temp'] . ' град. Цельсія'
-    ];
+        $humidityValue= [
+        '#type' => 'html_tag',
+        '#tag' => 'p',
+        '#value' => 'Вологість: ' . $responseParsed['main']['humidity'] . ' %'
+        ];
 
-    $humidityValue= [
-      '#type' => 'html_tag',
-      '#tag' => 'p',
-      '#value' => 'Вологість: ' . $responseParsed['main']['humidity'] . ' %'
-    ];
+        $weatherValue= [
+        '#type' => 'html_tag',
+        '#tag' => 'p',
+        '#value' => 'Погода: ' . $responseParsed['weather'][0]['description']
+        ];
 
-    $weatherValue= [
-      '#type' => 'html_tag',
-      '#tag' => 'p',
-      '#value' => 'Погода: ' . $responseParsed['weather'][0]['description']
-    ];
+        $windValue= [
+        '#type' => 'html_tag',
+        '#tag' => 'p',
+        '#value' => 'Вітер: ' . $responseParsed['wind']['speed'] . ' м/с'
+        ];
 
-    $windValue= [
-      '#type' => 'html_tag',
-      '#tag' => 'p',
-      '#value' => 'Вітер: ' . $responseParsed['wind']['speed'] . ' м/с'
-    ];
-
-    return [
-      $cityName,
-      $weatherValue,
-      $temperatureValue,
-      $humidityValue,
-      $windValue,
-    ];
-  }
-
+        return [
+        $cityName,
+        $weatherValue,
+        $temperatureValue,
+        $humidityValue,
+        $windValue,
+        ];
+    }
 }
-
